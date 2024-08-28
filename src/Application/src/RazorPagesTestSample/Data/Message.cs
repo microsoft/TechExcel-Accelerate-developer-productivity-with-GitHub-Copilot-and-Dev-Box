d62e7e6c6b3b2ca087a6370616872e0f4bf13a00
Puneet Devadiga <pdevadiga@microsoft.com>
2024-08-28 12:30:58 +0800

    change in chars

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -6,10 +6,9 @@ namespace RazorPagesTestSample.Data
     public class Message
     {
         public int Id { get; set; }
-
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

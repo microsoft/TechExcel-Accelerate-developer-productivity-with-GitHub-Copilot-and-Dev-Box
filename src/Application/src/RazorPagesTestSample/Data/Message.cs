cfd6b90c4974ec241628ac575a6a5f42e6b71220
Jose Bonnin <jbonnin@microsoft.com>
2024-09-10 14:32:47 +0200

    Added documentation and expanded string length to 250 chars

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -1,5 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <summary>
+/// Represents a message entity with an ID and text content.
+/// </summary>
+public class Message
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the message.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text content of the message.
+    /// </summary>
+    /// <remarks>
+    /// The text content is required and must be a string with a maximum length of 200 characters.
+    /// </remarks>
+    [Required]
+    [DataType(DataType.Text)]
+    [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
+    public string Text { get; set; }
+}
 namespace RazorPagesTestSample.Data
 {
     #region snippet1
@@ -9,7 +30,7 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

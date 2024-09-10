12e8fd8ad9b6e0ada6120e2b8e0445752c897e3a
System Administrator <admin@mngenvmcap025507.onmicrosoft.com>
2024-09-10 13:33:51 +0100

    Update text limit

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,16 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        /// <remarks>
+        /// This property is required and should contain text data.
+        /// The maximum length of the text is 250 characters.
+        /// </remarks>
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

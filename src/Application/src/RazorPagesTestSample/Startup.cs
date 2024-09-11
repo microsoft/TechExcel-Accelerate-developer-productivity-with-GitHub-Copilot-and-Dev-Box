1d14a068876c8f0824529fbbf7f097ec48dbfb70
Jose Bonnin <jbonnin@microsoft.com>
2024-09-11 12:29:45 +0200

    Update Startup.cs

--- src/Application/src/RazorPagesTestSample/Startup.cs
+++ src/Application/src/RazorPagesTestSample/Startup.cs
@@ -9,6 +9,7 @@ namespace RazorPagesTestSample
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));

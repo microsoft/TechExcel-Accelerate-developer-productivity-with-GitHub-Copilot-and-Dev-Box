e8e0a6e220633327e970e06cf29a7fa32c64cfe3
Bashar Abdelqader <bfayez@live.com>
2024-09-13 11:32:49 -0700

    Support for 300 chars

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,22 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        /// <value>
+        /// The text content of the message, limited to 300 characters.
+        /// </value>
+        /// <remarks>
+        /// This property is required and must be a text data type. If the text exceeds 300 characters,
+        /// an error message will be displayed indicating the character limit.
+        /// </remarks>
+        /// <exception cref="ValidationException">
+        /// Thrown when the text exceeds 300 characters or is not provided.
+        /// </exception>
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(300, ErrorMessage = "There's a 300 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

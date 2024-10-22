7200845b123913148a1075649cc46f6d9423c137
Pradeep Kumar <pradk@microsoft.com>
2024-10-22 13:43:23 -0700

    "Resolves #issue01"

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -9,6 +9,12 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
+        /// <summary>
+        /// Calculates the factorial of a given number.
+        /// </summary>
+        /// <param name="n">The number for which the factorial is to be calculated.</param>
+        /// <returns>The factorial of the given number.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the input number is less than 0.</exception>
         [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }

cc8e6a52ed4dbdc04a4f79a8329b1446e64ec945
qfchenMSFT <chenfa@microsoft.com>
2024-10-24 00:41:40 -0500

    more action

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -9,6 +9,12 @@ namespace RazorPagesTestSample.Data
 
         [Required]
         [DataType(DataType.Text)]
+            /// <summary>
+    /// Gets or sets the content of the message.
+    /// </summary>
+    /// <remarks>
+    /// The message content is limited to 200 characters.
+    /// </remarks>
         [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }

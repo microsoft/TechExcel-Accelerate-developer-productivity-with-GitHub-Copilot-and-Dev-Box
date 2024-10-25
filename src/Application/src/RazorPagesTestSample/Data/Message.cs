17d2eecfdcb9cb651c26279f6ddecf694c376a1a
Renzo Carpio <renzedu@gmail.com>
2024-10-24 21:52:36 -0400

    Refactor workflow file structure

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,18 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Gets or sets the message text.
+        /// </summary>
+        /// <value>
+        /// The text of the message, limited to 250 characters.
+        /// </value>
+        /// <remarks>
+        /// This property is required and must be a text data type. If the text exceeds 200 characters, an error message will be displayed.
+        /// </remarks>
+        /// /// /// /// /// /// /// /// /// [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

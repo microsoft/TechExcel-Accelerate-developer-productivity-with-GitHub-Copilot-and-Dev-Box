93328eecbef14f8c9abf2531548c83f293a78f80
Sayed Ali <sayed.ali@microsoft.com>
2024-10-29 14:37:58 +0100

    Resolves #6

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,7 +7,17 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        /// <value>
+        /// The text content of the message, limited to 200 characters.
+        /// </value>
+        /// <remarks>
+        /// This property is required and must be a text data type. 
+        /// If the text exceeds 200 characters, an error message will be displayed.
+        /// </remarks>
+        /// /// /// /// /// /// /// /// /// /// [Required]
         [DataType(DataType.Text)]
         [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }

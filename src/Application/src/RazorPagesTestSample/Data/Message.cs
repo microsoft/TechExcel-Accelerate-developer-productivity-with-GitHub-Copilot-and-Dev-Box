ed5103145461f3e3c8c3e2a43153049ceba876de
Frederic Wickert <frederic.wickert@outlook.fr>
2024-10-29 13:22:47 +0100

    resolves #3

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,19 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        /// <value>
+        /// The text content of the message, limited to 250 characters.
+        /// </value>
+        /// <remarks>
+        /// This property is required and must be a text data type. If the text exceeds 250 characters,
+        /// an error message will be displayed indicating the character limit.
+        /// </remarks>
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

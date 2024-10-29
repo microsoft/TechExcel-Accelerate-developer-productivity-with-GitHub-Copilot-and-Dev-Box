cb92614c36b04a3f3a121b3198cf67658c07b720
Sven Pohl <sven.pohl@microsoft.com>
2024-10-29 14:28:04 +0100

    Update character limit for message text field

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,9 +7,21 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
-        [Required]
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        /// <value>
+        /// The text content of the message, limited to 250 characters.
+        /// </value>
+        /// <remarks>
+        /// The text must be a valid string and is required. If the text exceeds 250 characters, an error message will be displayed.
+        /// </remarks>
+        /// <exception cref="ValidationException">
+        /// Thrown when the text exceeds 250 characters or is not provided.
+        /// </exception>
+        /// /// /// /// /// /// /// /// /// /// /// /// [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

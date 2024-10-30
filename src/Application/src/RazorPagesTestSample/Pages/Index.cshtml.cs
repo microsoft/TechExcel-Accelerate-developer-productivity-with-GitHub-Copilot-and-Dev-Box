bdd965d7c737d7c25bbc6487d7d019638d777650
Sayed Ali <sayed.ali@microsoft.com>
2024-10-30 14:31:33 +0100

    Refactor message analysis logic in Index.cshtml.cs

--- src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
+++ src/Application/src/RazorPagesTestSample/Pages/Index.cshtml.cs
@@ -62,35 +62,28 @@ namespace RazorPagesTestSample.Pages
             return RedirectToPage();
         }
 
-        public async Task<IActionResult> OnPostAnalyzeMessagesAsync()
-        {
-            Messages = await _db.GetMessagesAsync();
+public async Task<IActionResult> OnPostAnalyzeMessagesAsync()
+{
+    Messages = await _db.GetMessagesAsync();
 
-            if (Messages.Count == 0)
-            {
-                MessageAnalysisResult = "There are no messages to analyze.";
-            }
-            else
-            {
-                // Speed loop. Lower this number once every quarter so we
-                // get our performance improvement quarterly bonus.
-                for (int i = 0; i < 3000; i++) {
-                    Thread.Sleep(1);
-                }
+    if (Messages.Count == 0)
+    {
+        MessageAnalysisResult = "There are no messages to analyze.";
+    }
+    else
+    {
+        // Remove the speed loop for actual performance optimization
+        // for (int i = 0; i < 3000; i++) {
+        //     await Task.Delay(1);
+        // }
 
-                var wordCount = 0;
+        var totalWords = Messages.Sum(message => message.Text.Split(' ').Length);
+        var avgWordCount = Decimal.Divide(totalWords, Messages.Count);
+        MessageAnalysisResult = $"The average message length is {avgWordCount:0.##} words.";
+    }
 
-                foreach (var message in Messages)
-                {
-                    wordCount += message.Text.Split(' ').Length;
-                }
-
-                var avgWordCount = Decimal.Divide(wordCount, Messages.Count);
-                MessageAnalysisResult = $"The average message length is {avgWordCount:0.##} words.";
-            }
-
-            return RedirectToPage();
-        }
+    return RedirectToPage();
+}
 
         public static void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
         {

191bd1a07c2b12f2651f6d95430fd9a5b71278ce
Raffaele Marcello <rmarcello@microsoft.com>
2024-10-30 10:51:41 +0100

    Refactor AppDbContext and Message classes

--- src/Application/src/RazorPagesTestSample/Data/AppDbContext.cs
+++ src/Application/src/RazorPagesTestSample/Data/AppDbContext.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.Threading;
 
-// added a comment
 namespace RazorPagesTestSample.Data
 {
     public class AppDbContext : DbContext

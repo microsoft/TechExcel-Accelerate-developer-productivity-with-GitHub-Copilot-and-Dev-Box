2654eaf83a4d113ae44a771770ba87bd07e5ccb0
pythonalam <alamini@microsoft.com>
2025-01-17 12:51:25 -0500

    Resolving issue 2

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -3,13 +3,16 @@ using System.ComponentModel.DataAnnotations;
 namespace RazorPagesTestSample.Data
 {
     #region snippet1
+    /// <summary>
+    /// Represents a message with a maximum length of 250 characters.
+    /// </summary>
     public class Message
     {
         public int Id { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
-        [StringLength(200, ErrorMessage = "There's a 200 character limit on messages. Please shorten your message.")]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
         public string Text { get; set; }
     }
     #endregion

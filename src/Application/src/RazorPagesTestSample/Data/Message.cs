94bc6442a3c90c292ffd93032fac8e60e899a3ba
Student1-44192091 <Student1-44192091@LODSPRODMCA.onmicrosoft.com>
2025-01-17 13:06:06 -0600

    Documents perfomance issue message and # of characters

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -7,18 +7,23 @@ namespace RazorPagesTestSample.Data
     {
         public int Id { get; set; }
 
+
         /// <summary>
-        /// Gets or sets the text of the message.
+        /// Gets or sets the message text.
         /// </summary>
         /// <value>
-        /// The text content of the message, limited to 250 characters.
+        /// The message text.
         /// </value>
         /// <remarks>
-        /// This property is required and must be a text data type. If the text exceeds 250 characters, an error message will be displayed.
+        /// The text is required, must be of type text, and has a maximum length of 250 characters.
         /// </remarks>
         /// <exception cref="ValidationException">
         /// Thrown when the text exceeds the 250 character limit.
         /// </exception>
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
+        public string Text { get; set; }
     }
     #endregion
 }
